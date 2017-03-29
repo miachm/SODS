0ae8471184e0bba68f5c6eb15943ec9e6956d60b
Miguel <miachm3@gmail.com>
2017-03-29 14:39:03 +0100

    Uncompress the files of the ODS

--- src/OdsReader.java
+++ src/OdsReader.java
@@ -1,7 +1,10 @@
 package com.github.miachm.SODS;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
@@ -11,27 +14,38 @@ class OdsReader {
     static void load(InputStream in,SpreadSheet spread) throws IOException {
         /* TODO This code if for ods files in zip. But we could have XML-ONLY FILES */
 
+        Map<String,byte[]> files = uncompress(in);
+    }
+
+    private static Map<String,byte[]> uncompress(InputStream in) throws IOException {
+        Map<String,byte[]> map = new HashMap<String,byte[]>();
         ZipInputStream zip = new ZipInputStream(in);
         byte buff[] = new byte[TAM_BUFFER];
-        ZipEntry entry = zip.getNextEntry();
 
+        ZipEntry entry = zip.getNextEntry();
         while (entry != null){
-            processEntry(zip,buff,entry,spread);
+            processEntry(map,zip,buff,entry);
+            zip.closeEntry();
             entry = zip.getNextEntry();
         }
 
         zip.close();
+
+        return map;
     }
 
-    private static void processEntry(ZipInputStream zin, byte[] buff, ZipEntry entry, SpreadSheet spread) {
-        System.out.println(entry.getName());
+    private static void processEntry(Map<String,byte[]> map,ZipInputStream zin, byte[] buff,ZipEntry entry) throws IOException {
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        int len;
 
-        if (entry.getName().equals("Content.xml")){
-            processContent(entry);
+        while (true){
+            len = zin.read(buff);
+            if (len == -1) {
+                stream.close();
+                map.put(entry.getName(),stream.toByteArray());
+                return;
+            }
+            stream.write(buff,0,len);
         }
-    }
-
-    private static void processContent(ZipEntry entry) {
-    
     }
 }
