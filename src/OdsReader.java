418a2220956ede8ccf37935c3535f08ab0711203
Miguel <miachm3@gmail.com>
2017-03-29 15:21:49 +0100

    Aditional compatibility tests while reading the manifest

--- src/OdsReader.java
+++ src/OdsReader.java
@@ -1,5 +1,14 @@
 package com.github.miachm.SODS;
 
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -78,6 +87,40 @@ class OdsReader {
     }
 
     private static void readManifest(byte[] manifest) {
+        try{
+            DocumentBuilderFactory factory =
+                    DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            Document doc = builder.parse(new ByteArrayInputStream(manifest));
 
+            Element root = doc.getDocumentElement();
+            if (!root.getNodeName().equals("manifest:manifest")) {
+                throw new NotAnOds("The signature of the manifest is not valid. Is it an ODS file?");
+            }
+
+
+            NodeList files = doc.getElementsByTagName("manifest:file-entry");
+            iterateFilesEntriesManifest(files);
+
+        }catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        } catch (SAXException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void iterateFilesEntriesManifest(NodeList files){
+        for (int i = 0;i < files.getLength();i++) {
+            NodeList children = files.item(i).getChildNodes();
+
+            for (int j = 0;j <children.getLength();j++) {
+                if (children.item(j).getNodeName().equals("manifest:encryption-data")) {
+                    throw new OperationNotSupported("This file has encription technology that it's not supported" +
+                            "by this library");
+                }
+            }
+        }
     }
 }
