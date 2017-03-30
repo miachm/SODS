cc7eb32b0eeb0f932035c8f85bceb2d3a78159a9
Miguel <miachm3@gmail.com>
2017-03-30 12:18:19 +0100

    Move away the uncompress process to a different file

--- src/com/github/miachm/SODS/input/OdsReader.java
+++ src/com/github/miachm/SODS/input/OdsReader.java
@@ -1,8 +1,8 @@
 package com.github.miachm.SODS.com.github.miachm.SODS.input;
 
-import com.github.miachm.SODS.com.github.miachm.SODS.spreadsheet.SpreadSheet;
 import com.github.miachm.SODS.com.github.miachm.SODS.exceptions.NotAnOds;
 import com.github.miachm.SODS.com.github.miachm.SODS.exceptions.OperationNotSupported;
+import com.github.miachm.SODS.com.github.miachm.SODS.spreadsheet.SpreadSheet;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
@@ -12,62 +12,24 @@ import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.HashMap;
 import java.util.Map;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipInputStream;
 
 public class OdsReader {
-    private static final int TAM_BUFFER = 1000;
     private static final String CORRECT_MIMETYPE = "application/vnd.oasis.opendocument.spreadsheet";
     private static final String MANIFEST_PATH = "META-INF/manifest.xml";
 
     static public void load(InputStream in,SpreadSheet spread) throws IOException {
         /* TODO This code if for ods files in zip. But we could have XML-ONLY FILES */
 
-        Map<String,byte[]> files = uncompress(in);
+        Uncompressor uncompressor = new Uncompressor(in);
+        Map<String,byte[]> files = uncompressor.getFiles();
 
         checkMimeType(files);
 
         byte[] manifest = getManifest(files);
         readManifest(manifest);
-    }
-
-    private static Map<String,byte[]> uncompress(InputStream in) throws IOException {
-        Map<String,byte[]> map = new HashMap<String,byte[]>();
-        ZipInputStream zip = new ZipInputStream(in);
-        byte buff[] = new byte[TAM_BUFFER];
-
-        ZipEntry entry = zip.getNextEntry();
-        while (entry != null){
-            if (!entry.isDirectory()) {
-                processEntry(map, zip, buff, entry);
-                zip.closeEntry();
-            }
-            entry = zip.getNextEntry();
-        }
-
-        zip.close();
-
-        return map;
-    }
-
-    private static void processEntry(Map<String,byte[]> map,ZipInputStream zin, byte[] buff,ZipEntry entry) throws IOException {
-        ByteArrayOutputStream stream = new ByteArrayOutputStream();
-        int len;
-
-        while (true){
-            len = zin.read(buff);
-            if (len == -1) {
-                stream.close();
-                map.put(entry.getName(),stream.toByteArray());
-                return;
-            }
-            stream.write(buff,0,len);
-        }
     }
 
     private static void checkMimeType(Map<String,byte[]> map){
---
+++ src/com/github/miachm/SODS/input/Uncompressor.java
@@ -0,0 +1,54 @@
+package com.github.miachm.SODS.com.github.miachm.SODS.input;
+
+import java.io.ByteArrayOutputStream;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
+class Uncompressor implements Closeable {
+
+    private static final int TAM_BUFFER = 1000;
+    private byte buff[] = new byte[TAM_BUFFER];
+    private Map<String,byte[]> map = new HashMap<String,byte[]>();
+    private ZipInputStream zip;
+
+    Uncompressor(InputStream in){
+        this.zip = new ZipInputStream(in);
+    }
+
+    Map<String,byte[]> getFiles() throws IOException {
+        ZipEntry entry = zip.getNextEntry();
+        while (entry != null) {
+            if (!entry.isDirectory()) {
+                processEntry(entry);
+                zip.closeEntry();
+            }
+            entry = zip.getNextEntry();
+        }
+        return map;
+    }
+
+    private void processEntry(ZipEntry entry) throws IOException {
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        int len;
+
+        while (true){
+            len = zip.read(buff);
+            if (len == -1) {
+                stream.close();
+                map.put(entry.getName(),stream.toByteArray());
+                return;
+            }
+            stream.write(buff,0,len);
+        }
+    }
+
+    @Override
+    public void close() throws IOException{
+        zip.close();
+    }
+}
