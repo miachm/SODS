75dc63ae945de729361a6cc6001c1a32ce125d1e
Miguel <miachm3@gmail.com>
2017-04-24 00:09:29 +0100

    Implemented testGetRow()

--- tests/com/github/miachm/SODS/spreadsheet/RangeTest.java
+++ tests/com/github/miachm/SODS/spreadsheet/RangeTest.java
@@ -149,7 +149,16 @@ public class RangeTest {
 
     @Test
     public void testGetRow() throws Exception {
+        Sheet sheet = new Sheet("A");
+        sheet.appendRows(10);
+        sheet.appendColumns(10);
 
+        for (int i = 0;i < sheet.getMaxRows()/2;i++) {
+            for (int j = 0; j < sheet.getMaxColumns() / 2; j++) {
+                Range range = sheet.getRange(i, j, 2, 2);
+                assertEquals(range.getRow(), i);
+            }
+        }
     }
 
     @Test
