ab4fda770160c01122b17048ed32394b8b297303
Miguel <miachm3@gmail.com>
2017-04-25 23:48:14 +0100

    Remove unnecesary tag in Manifest of SaveODS

--- src/com/github/miachm/SODS/input/OdsWritter.java
+++ src/com/github/miachm/SODS/input/OdsWritter.java
@@ -68,7 +68,6 @@ public class OdsWritter {
                 tr.setOutputProperty(OutputKeys.INDENT, "yes");
                 tr.setOutputProperty(OutputKeys.METHOD, "xml");
                 tr.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
-                tr.setOutputProperty(OutputKeys.DOCTYPE_SYSTEM, "roles.dtd");
                 tr.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
 
                 ByteArrayOutputStream o = new ByteArrayOutputStream();
