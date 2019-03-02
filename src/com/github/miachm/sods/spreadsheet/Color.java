22730c7ebeeba3f886e312eb4d7ae7c72e15a9af
Miguel <miachm3@gmail.com>
2019-03-02 12:39:22 +0100

    FIx color representation Add javadocs to the Color class

--- src/com/github/miachm/sods/spreadsheet/Color.java
+++ src/com/github/miachm/sods/spreadsheet/Color.java
@@ -1,21 +1,41 @@
 package com.github.miachm.sods.spreadsheet;
 
+/** Color is a inmutable class for represent colors.
+ */
+
 final public class Color implements Cloneable {
     private final int red;
-    private final int blue;
     private final int green;
+    private final int blue;
 
-    public Color(int red, int blue, int green) {
+    /**
+     * It creates a color object from a RGB format (red, green, blue).
+     *
+     * @param red the red color intensity. It must be in a 0-255 range
+     * @param green the green color intensity. It must be in a 0-255 range
+     * @param blue the blue color intensity. It must be in a 0-255 range
+     */
+    public Color(int red, int green, int blue) {
         if (red < 0 || red > 255 ||
-                blue < 0 || blue > 255 ||
-                green < 0 || green > 255)
+                green < 0 || green > 255 ||
+                blue < 0 || blue > 255)
             throw new IllegalArgumentException("Error, parameters out of range (0-255)");
 
         this.red = red;
-        this.blue = blue;
         this.green = green;
+        this.blue = blue;
     }
 
+    /**
+     * It creates a color object from a String representation (CSS Style).
+     * The first character is the hashtag '#'
+     * The next two characters are the red intensity in hex (ex: A4)
+     * The next two characters are the green intensity in hex (ex: 3B)
+     * The next two characters are the blue intensity in hex (ex: 53)
+     *
+     * @param hexform The string representation of the color. For example: "#A24C21"
+     * @throws IllegalArgumentException if the string hasn't 7 characters or is ill-formed
+     */
     public Color(String hexform)
     {
         if (hexform.length() != 7) {
@@ -23,8 +43,8 @@ final public class Color implements Cloneable {
         }
 
         this.red = Integer.valueOf(hexform.substring(1, 3), 16);
-        this.blue = Integer.valueOf(hexform.substring(3, 5), 16);
-        this.green = Integer.valueOf(hexform.substring(5, 7), 16);
+        this.green = Integer.valueOf(hexform.substring(3, 5), 16);
+        this.blue = Integer.valueOf(hexform.substring(5, 7), 16);
     }
 
     public int getRed() {
@@ -50,10 +70,16 @@ final public class Color implements Cloneable {
         return text;
     }
 
+    /**
+     * A string representation of the color
+     *
+     * @return A string of 7 characters with the hex representation of the color (ex: #4a1a4b)
+     */
+
     @Override
     public String toString()
     {
-        return "#" + fill(Integer.toHexString(red),2) + fill(Integer.toHexString(blue), 2) + fill(Integer.toHexString(green), 2);
+        return "#" + fill(Integer.toHexString(red),2) + fill(Integer.toHexString(green), 2) + fill(Integer.toHexString(blue), 2);
     }
 
     @Override
