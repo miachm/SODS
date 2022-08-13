ab2b1baac14030e8df4444aba16fe2106798c9d5
Miguel <miachm3@gmail.com>
2022-08-13 15:47:32 +0100

    Fix bug when multiple rows are hidden

--- src/com/github/miachm/sods/OdsReader.java
+++ src/com/github/miachm/sods/OdsReader.java
@@ -318,7 +318,8 @@ class OdsReader {
 
                     String visibility = instance.getAttribValue("table:visibility");
                     if ("collapse".equals(visibility))
-                        sheet.hideRow(sheet.getMaxRows()-1);
+                        for (int i = 0; i < numRows; i++)
+                            sheet.hideRow(sheet.getMaxRows() - i - 1);
 
                     String rowStyleName = instance.getAttribValue("table:style-name");
                     if (rowStyleName != null && numRows < 1000) {
